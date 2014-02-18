7f1bf38435896a6ae5ea20eb3b3878136d36c43e
Thiago Rocha <thiago.rocha@liferay.com>
2014-02-18 20:27:26 -0300

    rename ImageDiff.js to ImageComparator.js

--- ImageComparator.js
+++ ImageComparator.js
@@ -1,13 +1,13 @@
-function ImageDiff() {
+function ImageComparator() {
     this._canvas = document.createElement('canvas');
     this._canvasContext = this._canvas.getContext('2d');
 }
 
-ImageDiff.prototype._canvas = null;
+ImageComparator.prototype._canvas = null;
 
-ImageDiff.prototype._canvasContext = null;
+ImageComparator.prototype._canvasContext = null;
 
-ImageDiff.prototype.find = function(imgSrc1, imgSrc2, threshold, callback) {
+ImageComparator.prototype.compare = function(imgSrc1, imgSrc2, threshold, callback) {
     var instance = this,
         image1 = new Image(),
         image2 = new Image(),
@@ -31,7 +31,7 @@ ImageDiff.prototype.find = function(imgSrc1, imgSrc2, threshold, callback) {
     };
 };
 
-ImageDiff.prototype._drawAbutImages = function(image1, image2) {
+ImageComparator.prototype._drawAbutImages = function(image1, image2) {
     var instance = this,
         canvas = instance._canvas,
         context = instance._canvasContext;
@@ -42,7 +42,7 @@ ImageDiff.prototype._drawAbutImages = function(image1, image2) {
     context.drawImage(image2, 0, image1.height);
 };
 
-ImageDiff.prototype._scan = function(image1, image2, threshold) {
+ImageComparator.prototype._scan = function(image1, image2, threshold) {
     var instance = this,
         context = instance._canvasContext,
 
@@ -63,3 +63,9 @@ ImageDiff.prototype._scan = function(image1, image2, threshold) {
     }
     return 0;
 };
+
+var imageComparator = new ImageComparator();
+imageComparator.compare('file:///Users/thiagorocha/Projects/Image-Diff/images/export_import_1.png', 'file:///Users/thiagorocha/Projects/Image-Diff/images/export_import_2.png', 0.001, function(result) {
+    console.log(result);
+    phantom.exit();
+});
